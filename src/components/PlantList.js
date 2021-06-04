cc793c3399bf3b7a21cf24c2daa122a13f817d05
davedwight <dpdwight@gmail.com>
2021-06-04 16:24:47 -0600

    Finished axios call and built custom hook

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,6 +3,9 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  state = {
+    plants: []
+  };
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
@@ -12,7 +15,15 @@ export default class PlantList extends Component {
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
+        console.log("axios call worked")
         console.log(res);
+        this.setState({
+          ...this.state,
+          plants: res.data
+        })
+      })
+      .catch(err => {
+        console.log("call failed:", err);
       });
   }
 
